f5ad43fb62382d7ddebba754417db76c7f192ad2
Onur Uyar <me@onuruyar.com>
2014-08-25 12:27:13 +0100

    minor fixxx

--- pinger.rb
+++ pinger.rb
@@ -6,9 +6,9 @@ include Clockwork
 @sites = File.read("./sites.csv").split("\n")
 
 every(15.minutes, 'ping single dyno apps to prevent idling') { 
-  @sites.split(",").each {|url| 
+  @sites.each {|url| 
     Thread.new{
-      puts "URL: #{url}, STATUS: #{HTTParty.get(url).headers["status"]}"
+      puts "URL: #{url}, STATUS: #{HTTParty.get(url).response.inspect}"
     }.run
   }
 }
